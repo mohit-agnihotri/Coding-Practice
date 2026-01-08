90f8994114c39634e11fa9ac4f6b3f3e0a330ea0
mohit-agnihotri <mohitagnihotri1320@gmail.com>
2026-01-08 22:29:11 +0530

    Time: 0 ms (100%), Space: 43 MB (28.79%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -4,11 +4,6 @@ class Solution {
         if (s.length() != goal.length())
             return false;
         String check = s + s;
-        for (int i = 0; i < check.length() - goal.length(); i++) {
-            if (check.substring(i, i + goal.length()).equals(goal)) {
-                return true;
-            }
-        }
-        return false;
+        return check.contains(goal);
     }
 }
