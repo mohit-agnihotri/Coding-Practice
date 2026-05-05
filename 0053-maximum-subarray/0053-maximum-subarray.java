049e087ee868620ce1a7283110009f13a93d503c
Mohit Agnihotri <mohitagnihotri1320@gmail.com>
2026-05-05 23:58:31 +0530

    Time: 1 ms (99.97%), Space: 77 MB (91.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,30 @@
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int n = nums.length ;
+//         int currSum = 0;
+//         int maxSum = Integer.MIN_VALUE;
+//         for(int i = 0 ; i < n ; i++){
+//             currSum += nums[i];
+//             if(currSum > maxSum ){
+//                 maxSum = currSum;
+//             }
+//             if(currSum < 0){
+//                 currSum = 0 ;
+//             }
+//         }
+//         return maxSum;
+//     }
+// }
+
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int n = nums.length ;
-        int currSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for(int i = 0 ; i < n ; i++){
-            currSum += nums[i];
-            if(currSum > maxSum ){
-                maxSum = currSum;
-            }
-            if(currSum < 0){
-                currSum = 0 ;
-            }
+        int currMax = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1 ; i < n ; i++){
+           currMax = Math.max(currMax + nums[i] , nums[i]);
+           maxSum = Math.max(maxSum , currMax);
         }
         return maxSum;
     }
