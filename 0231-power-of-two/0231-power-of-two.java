43511f334f86b523e010ee2ac6d52412e28bd367
Mohit Agnihotri <mohitagnihotri1320@gmail.com>
2026-08-15 00:13:00 +0530

    Time: 1 ms (96.59%), Space: 42.5 MB (56.15%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,6 +3,11 @@ class Solution {
         if(n <= 0){
             return false;
         }
-        return (n & (n -1)) == 0;
+        // return (n & (n -1)) == 0;
+
+        while(n % 2 == 0){
+            n = n / 2;
+        }
+        return n == 1;
     }
 }
