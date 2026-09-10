c09c5470507c1f4d57323c744b355b199ca97343
Mohit Agnihotri <mohitagnihotri1320@gmail.com>
2026-09-10 22:27:30 +0530

    Time: 7 ms (58.38%), Space: 48.2 MB (52.88%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,27 +14,49 @@
  * }
  */
 class Solution {
-    int depth = 0 ; 
-    int maxDepth = 0 ;
-    int sum = 0 ;
+    // int depth = 0 ; 
+    // int maxDepth = 0 ;
+    // int sum = 0 ;
     public int deepestLeavesSum(TreeNode root) {
-        traverse(root);
-        return sum ;
-    }
-    void traverse(TreeNode root){
-        if(root == null) return ;
-        depth++;
-        traverse(root.left);
-        traverse(root.right);
+        // traverse(root);
+        // return sum ;
 
-        if(root.left == null && root.right == null){
-            if(depth > maxDepth){
-                maxDepth = depth;
-                sum = root.val;
-            } else if(depth == maxDepth){
-                sum += root.val;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int sum = 0 ;
+        while(!q.isEmpty()){
+            int size = q.size();
+            sum = 0;
+            while(size > 0){
+                TreeNode node = q.poll();
+                sum += node.val;
+                 if (node.left != null) {
+                    q.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+                size--;
             }
         }
-        depth--;
+            return sum;
+        }
     }
-}
+//     void traverse(TreeNode root){
+//         if(root == null) return ;
+//         depth++;
+//         traverse(root.left);
+//         traverse(root.right);
+
+//         if(root.left == null && root.right == null){
+//             if(depth > maxDepth){
+//                 maxDepth = depth;
+//                 sum = root.val;
+//             } else if(depth == maxDepth){
+//                 sum += root.val;
+//             }
+//         }
+//         depth--;
+//     }
+// }
